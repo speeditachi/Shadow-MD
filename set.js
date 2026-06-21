86c4eab9f5330b556084d321107b4cc2b2cf9835
speeditachi <razagithub21@gmail.com>
2026-06-21 14:58:53 -0500

    Create set.js

---
+++ set.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // 🤖 Bot Settings
+    botName: "Shadow MD",
+    prefix: ".",
+
+    // 👑 Owner Settings
+    ownerNumber: "509XXXXXXXX",
+
+    // ⚙️ Mode Settings
+    mode: "public", // public / private
+
+    // 🔥 System Settings
+    autoRead: true,
+    autoTyping: false,
+
+    // 💀 Bug / Advanced options (optional)
+    antiDelete: false,
+    autoReact: true
+}
